a309ffcd85fd0756b6d30f5982a993673a1a62ab
Vinícius Marini <marini_costa@hotmail.com>
2022-03-19 09:53:37 -0300

    css feito

--- Codigo/dindin-frontend/pages/login.vue
+++ Codigo/dindin-frontend/pages/login.vue
@@ -1,33 +1,37 @@
 <template>
   <div class="container">
     <form class="formLogin" @submit.prevent="handleSubmit">
-      <img src="@/static/logoComEscrito.svg" />
-      <div class="form-group">
-        <input
-          type="text"
-          v-model="email"
-          name="email"
-          class="form-control"
-          placeholder="EMAIL"
-        />
+      <div class="imgPlace">
+        <img src="@/static/logoComEscrito.svg" />
       </div>
-      <div class="form-group">
-        <input
-          type="password"
-          v-model="senha"
-          name="senha"
-          class="form-control"
-          placeholder="SENHA"
-        />
-      </div>
-      <div class="buttonArea">
-        <button v-on:click="handleSubmit()" class="btn btn-primary">
-          Acessar
-        </button>
-      </div>
-      <div>
-        <input type="checkbox" id="checkbox" v-model="rememberPass" />
-        <span>LEMBRAR SENHA</span>
+      <div class="formActions">
+        <div class="form-group">
+          <input
+            type="text"
+            v-model="email"
+            name="email"
+            class="form-control"
+            placeholder="EMAIL"
+          />
+        </div>
+        <div class="form-group">
+          <input
+            type="password"
+            v-model="senha"
+            name="senha"
+            class="form-control"
+            placeholder="SENHA"
+          />
+        </div>
+        <div class="buttonArea">
+          <button v-on:click="handleSubmit()" class="btn btn-primary">
+            ACESSAR
+          </button>
+        </div>
+        <div class="check">
+          <input type="checkbox" id="checkbox" v-model="rememberPass" />
+          <span>LEMBRAR SENHA</span>
+        </div>
       </div>
     </form>
   </div>
@@ -56,22 +60,47 @@ export default {
 </script>
 
 <style lang="css" scoped>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap");
 .container {
-  background-color: black;
+  width: 50%;
+  height: 50%;
+  margin: auto;
+  padding: 5%;
 }
 .formLogin {
   background-color: white;
+  font-family: "Roboto", sans-serif;
 }
+.imgPlace {
+  display: flex;
+  justify-content: center;
+}
+.imgPlace img {
+  width: 60%;
+}
+.formActions{
+  display:grid;
+}
+
 .form-group input {
-  width: 80%;
+  grid-row:1;
+  width: 100%;
   margin-bottom: 10px;
+  border: 1px solid grey;
+  height: 5vh;
+}
+
+.check{
+  display:flex;
 }
 button {
-  width: 80%;
+  width: 100%;
   margin-bottom: 10px;
   border: 0px;
-  height: 30px;
+  height: 5vh;
   background-color: #5bd098;
   color: white;
+  font-family: "Roboto", sans-serif;
+  font-weight: bold;
 }
 </style>
