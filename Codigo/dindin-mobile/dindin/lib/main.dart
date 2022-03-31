7db602ff8b7e61f3ccbc6103d6d44e418451435f
Vinicius Marini Costa E Oliveira <475997@sga.pucminas.br>
2022-03-31 18:25:16 -0300

    prevent rotate

--- Codigo/dindin-mobile/dindin/lib/main.dart
+++ Codigo/dindin-mobile/dindin/lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'pages/login2.dart';
 import 'pages/login.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   runApp(const MyApp());
 }
 
