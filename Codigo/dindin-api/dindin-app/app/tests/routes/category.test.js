8177a32466df00b54c0068479f3cdc19b51de84e
Vinícius Marini <marini_costa@hotmail.com>
2022-04-04 08:54:18 -0300

    tests added

--- Codigo/dindin-api/dindin-app/app/tests/routes/category.test.js
+++ Codigo/dindin-api/dindin-app/app/tests/routes/category.test.js
@@ -4,6 +4,8 @@ require("dotenv").config();
 const app = require('../../');
 const { connect, close } = require('../../database');
 
+const Category = require('../../models/Category')
+
 beforeAll(async () => {
     await connect();
 });
@@ -23,14 +25,46 @@ describe('Category API', () => {
         expect(response.statusCode).toEqual(201);
         expect(response.body).toHaveProperty('id');
     })
-    it('should show a category /api/category/1', async () =>{
+
+
+    it('should not create a new category', async () => {
+        const response = await supertest(app)
+            .post('/api/category')
+            .send({
+                name: 'Category',
+                color: 'Black'
+            });
+        expect(response.statusCode).toEqual(422);
+    })
+
+    it("should update a category", async () => {
+        const mockCategory = {
+            name: 'Category',
+            color: 'BLACK'
+        }
+        const createdCategory = await Category.create(mockCategory);
+
+        mockCategory.name='Category Updated';
+        mockCategory.color='GREEN'
+
+        const response = await supertest(app)
+            .put('/api/Category/' + createdCategory.id)
+            .send(mockCategory);
+
+        expect(response.statusCode).toEqual(201);
+        expect(response.body.name).toEqual(mockCategory.name)
+        expect(response.body.color).toEqual(mockCategory.color)
+
+    })
+
+    it('should show a category /api/category/1', async () => {
         const response = await supertest(app).get('/api/category/1');
         expect(response.statusCode).toEqual(200);
         expect(response.body).toHaveProperty('id');
     })
 
 
-    it('should get all categories', async() =>{
+    it('should get all categories', async () => {
         const response = await supertest(app)
             .get('/api/category');
         expect(response.statusCode).toEqual(200);
