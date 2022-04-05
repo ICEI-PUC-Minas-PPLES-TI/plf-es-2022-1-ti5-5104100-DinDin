75807184c55c54185c6415da5285f6e0dfd26d6b
Vinicius Marini Costa E Oliveira <475997@sga.pucminas.br>
2022-04-04 22:07:20 -0300

    buttons

--- Codigo/dindin-mobile/dindin/lib/pages/transaction.dart
+++ Codigo/dindin-mobile/dindin/lib/pages/transaction.dart
@@ -18,7 +18,40 @@ class _TransactionState extends State<Transaction> {
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Column(children: [
-            
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Wrap(
+                  children: [
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Income",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.lightGreen)),
+                    ),
+                  ],
+                ),
+                Wrap(
+                  children: [
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Outcome",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.red)),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ]),
         ),
       ),
