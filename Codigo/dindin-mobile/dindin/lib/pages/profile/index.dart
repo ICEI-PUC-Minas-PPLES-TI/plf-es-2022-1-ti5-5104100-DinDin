dfc16743e530855f90bab95103ee19975be676a6
Lucas Angelo O M Rocha <lcs2001_@hotmail.com>
2022-04-07 00:22:46 -0300

    Update do Scaffold

--- Codigo/dindin-mobile/dindin/lib/pages/profile/index.dart
+++ Codigo/dindin-mobile/dindin/lib/pages/profile/index.dart
@@ -14,88 +14,85 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     const primaryColor = Colors.green;
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Edit Profile'),
-          backgroundColor: primaryColor,
-        ),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 32.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                const SizedBox(height: 60),
-                TextFormField(
-                  initialValue: "Demo User 01",
-                  maxLength: 100,
-                  decoration: const InputDecoration(
-                    labelText: 'Name',
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(),
-                    ),
-                    suffixIcon: Icon(Icons.edit_outlined),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Edit Profile'),
+        backgroundColor: primaryColor,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 32.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const SizedBox(height: 60),
+              TextFormField(
+                initialValue: "Demo User 01",
+                maxLength: 100,
+                decoration: const InputDecoration(
+                  labelText: 'Name',
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(),
                   ),
+                  suffixIcon: Icon(Icons.edit_outlined),
                 ),
-                const SizedBox(
-                  height: 5,
-                ),
-                TextFormField(
-                  initialValue: "demo-user-01@email.com",
-                  maxLength: 150,
-                  decoration: const InputDecoration(
-                    labelText: 'E-mail',
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(),
-                    ),
-                    suffixIcon: Icon(Icons.edit_outlined),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              TextFormField(
+                initialValue: "demo-user-01@email.com",
+                maxLength: 150,
+                decoration: const InputDecoration(
+                  labelText: 'E-mail',
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(),
                   ),
+                  suffixIcon: Icon(Icons.edit_outlined),
                 ),
-                const SizedBox(
-                  height: 5,
-                ),
-                IntlPhoneField(
-                  initialValue: "31988887777",
-                  decoration: const InputDecoration(
-                    labelText: 'Phone Number',
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(),
-                    ),
-                    suffixIcon: Icon(Icons.edit_outlined),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              IntlPhoneField(
+                initialValue: "31988887777",
+                decoration: const InputDecoration(
+                  labelText: 'Phone Number',
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(),
                   ),
-                  initialCountryCode: 'BR',
-                  onChanged: (phone) {
-                    print(phone.completeNumber);
-                  },
-                  onCountryChanged: (country) {
-                    print('Country changed to: ' + country.name);
-                  },
+                  suffixIcon: Icon(Icons.edit_outlined),
                 ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    MaterialButton(
-                      minWidth: double.infinity,
-                      padding: const EdgeInsets.symmetric(vertical: 20),
-                      child: const Text('Save Changes'),
-                      color: primaryColor,
-                      textColor: Colors.white,
-                      onPressed: () {
-                        _formKey.currentState?.validate();
-                      },
-                    ),
-                    Container(height: 20.0),
-                  ],
-                ),
-              ],
-            ),
+                initialCountryCode: 'BR',
+                onChanged: (phone) {
+                  print(phone.completeNumber);
+                },
+                onCountryChanged: (country) {
+                  print('Country changed to: ' + country.name);
+                },
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  MaterialButton(
+                    minWidth: double.infinity,
+                    padding: const EdgeInsets.symmetric(vertical: 20),
+                    child: const Text('Save Changes'),
+                    color: primaryColor,
+                    textColor: Colors.white,
+                    onPressed: () {
+                      _formKey.currentState?.validate();
+                    },
+                  ),
+                  Container(height: 20.0),
+                ],
+              ),
+            ],
           ),
         ),
       ),
