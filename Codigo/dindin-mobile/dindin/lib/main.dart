e75f132e55ea4cd5be8801873bb876a4a32c883e
Vinícius Marini <marini_costa@hotmail.com>
2022-04-07 09:09:04 -0300

    to stop conflicts

--- Codigo/dindin-mobile/dindin/lib/main.dart
+++ Codigo/dindin-mobile/dindin/lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'pages/login2.dart';
-import 'pages/login.dart';
-import 'pages/transaction.dart';
 import 'package:flutter/services.dart';
 
 void main() {
@@ -16,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Transaction(),
+      home: Login2(),
     );
   }
 }
