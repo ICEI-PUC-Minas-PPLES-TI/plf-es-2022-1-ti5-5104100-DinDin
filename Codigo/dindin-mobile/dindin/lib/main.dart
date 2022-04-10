66f4b0f42e43bcca4d23d4418147704ee707f00d
Victor Campos <victorboaventcampos@gmail.com>
2022-04-10 17:07:15 -0300

    Page Create Wallet ok

--- Codigo/dindin-mobile/dindin/lib/main.dart
+++ Codigo/dindin-mobile/dindin/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:dindin/pages/goal/index.dart';
+import 'package:dindin/pages/wallet/create.dart';
 import 'package:flutter/material.dart';
 import 'package:dindin/pages/wallet/index.dart';
 import 'pages/login.dart';
@@ -20,6 +21,7 @@ class MyApp extends StatelessWidget {
       routes: {
         "/login": (context) => Login(),
         "/wallet/index": (context) => const WalletList(),
+        "/wallet/create": (context) => const WalletCreate(),
         "/goal/index": (context) => const GoalList()
       },
       theme: ThemeData(primarySwatch: Colors.green),
