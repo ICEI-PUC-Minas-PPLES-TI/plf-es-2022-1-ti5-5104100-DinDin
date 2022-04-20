aaea963a0b1da15af43a3d15a8acaeeec8908a54
Lucas Angelo O M Rocha <lcs2001_@hotmail.com>
2022-04-20 20:11:22 -0300

    Fix database creation

--- Codigo/dindin-api/dindin-app/app/config/config.js
+++ Codigo/dindin-api/dindin-app/app/config/config.js
@@ -10,7 +10,7 @@ module.exports = {
   development: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME_TEST,
+    database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     underscored: true,
@@ -33,7 +33,7 @@ module.exports = {
   test: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    database: process.env.DB_NAME_TEST,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     underscored: true,
