994227b3c3c65bab8ce6a77c8a5b82d638c1d54e
Lucas Angelo O M Rocha <lcs2001_@hotmail.com>
2022-04-21 16:57:00 -0300

    Create User Account Creation Test Suite

--- Codigo/dindin-api/dindin-app/app/tests/usecases/user/CreateUserAccountController.test.js
+++ Codigo/dindin-api/dindin-app/app/tests/usecases/user/CreateUserAccountController.test.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const app = require('../../..');
 const { connect, close } = require('../../../database');
 const User = require('../../../models/User');
+const FindUserAccountUseCase = require('../../../usecases/user/findAccount/FindUserAccountUseCase');
 
 beforeAll(async () => {
   await connect();
@@ -15,16 +16,136 @@ afterAll(async () => {
 
 describe('POST /user test suite', () => {
 
-  it('should create a new user', async () => {
+  it('should create a new user with success', async () => {
     const response = await supertest(app)
       .post('/api/user')
       .send({
-        name: 'Lcs',
-        email: `${Math.random()}@protonmail.com`,
-        password: 'mySuperSecretPassword'
+        name: 'User teste',
+        email: `${Math.random()}@email.com`,
+        password: 'userTestePassword'
       });
     expect(response.statusCode).toEqual(201);
     expect(response.body).toHaveProperty('user.id');
-  })
+  });
+
+  it('should fail to create user as it did not send the name field', async () => {
+    const response = await supertest(app)
+      .post('/api/user')
+      .send({
+        email: `${Math.random()}@email.com`,
+        password: 'userTestePassword'
+      });
+    expect(response.statusCode).toEqual(422);
+  });
+
+  it('should fail to create user with empty name field', async () => {
+    const response = await supertest(app)
+      .post('/api/user')
+      .send({
+        name: '',
+        email: `${Math.random()}@email.com`,
+        password: 'userTestePassword'
+      });
+      expect(response.statusCode).toEqual(422);
+  });
+
+  it('should fail to create user as it did not send the email field', async () => {
+    const response = await supertest(app)
+      .post('/api/user')
+      .send({
+        name: 'User teste',
+        password: 'userTestePassword'
+      });
+    expect(response.statusCode).toEqual(422);
+  });
+
+  it('should fail to create user with empty email field', async () => {
+    const response = await supertest(app)
+      .post('/api/user')
+      .send({
+        name: 'User teste',
+        email: '',
+        password: 'userTestePassword'
+      });
+      expect(response.statusCode).toEqual(422);
+  });
+
+  it('should fail to create user as it did not send the password field', async () => {
+    const response = await supertest(app)
+      .post('/api/user')
+      .send({
+        name: 'User teste',
+        email: `${Math.random()}@email.com`,
+      });
+    expect(response.statusCode).toEqual(422);
+  });
+
+  it('should fail to create user with empty password field', async () => {
+    const response = await supertest(app)
+      .post('/api/user')
+      .send({
+        name: 'User teste',
+        email: `${Math.random()}@email.com`,
+        password: ''
+      });
+      expect(response.statusCode).toEqual(422);
+  });
+
+  it('should fail to create user with invalid email field', async () => {
+    const response = await supertest(app)
+      .post('/api/user')
+      .send({
+        name: 'User teste',
+        email: "invalidEmail.com",
+        password: 'userTestePassword'
+      });
+    expect(response.statusCode).toEqual(422);
+  });
+
+  it('should fail to create user with name field more than 100 characters', async () => {
+    const response = await supertest(app)
+      .post('/api/user')
+      .send({
+        name: '12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901',
+        email: `${Math.random()}@email.com`,
+        password: '1234567'
+      });
+    expect(response.statusCode).toEqual(422);
+  });
+
+  it('should fail to create user with email field more than 150 characters', async () => {
+    const response = await supertest(app)
+      .post('/api/user')
+      .send({
+        name: 'User teste',
+        email: `${Math.random()}@1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901.com`,
+        password: '1234567'
+      });
+    expect(response.statusCode).toEqual(422);
+  });
+
+  it('should fail to create user with password field less than 8 characters', async () => {
+    const response = await supertest(app)
+      .post('/api/user')
+      .send({
+        name: 'User teste',
+        email: `${Math.random()}@email.com`,
+        password: '1234567'
+      });
+    expect(response.statusCode).toEqual(422);
+  });
+
+  it('should fail to create user with already used email', async () => {
+    const response = await supertest(app)
+      .post('/api/user')
+      .send({
+        name: 'User teste',
+        email: `${Math.random()}@email.com`,
+        password: 'userTestePassword'
+      });
+    expect(response.statusCode).toEqual(201);
+    expect(response.body).toHaveProperty('user.id');
+    const user = FindUserAccountUseCase
+  });
 
 })
