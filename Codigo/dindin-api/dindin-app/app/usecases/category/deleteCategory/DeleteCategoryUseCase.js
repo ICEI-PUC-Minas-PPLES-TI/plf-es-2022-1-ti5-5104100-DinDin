5cd8268aba7093db664331d6fa4dec09b11fb5e7
Lucas Angelo O M Rocha <lcs2001_@hotmail.com>
2022-04-24 23:45:19 -0300

    Fix lint

--- Codigo/dindin-api/dindin-app/app/usecases/category/deleteCategory/DeleteCategoryUseCase.js
+++ Codigo/dindin-api/dindin-app/app/usecases/category/deleteCategory/DeleteCategoryUseCase.js
@@ -2,12 +2,12 @@ const AppError = require("../../../errors/AppError");
 const FindCategoryUseCase = require("../findCategory/FindCategoryUseCase");
 class DeleteCategoryUseCase {
     async delete(id) {
-      const findCategoryUseCase = new FindCategoryUseCase();
-      const category = await findCategoryUseCase.find(id);
-      await category.destroy().catch((error) => {
-        throw new AppError("Erro interno do servidor!", 500, error);
-      });
-      return category;
+        const findCategoryUseCase = new FindCategoryUseCase();
+        const category = await findCategoryUseCase.find(id);
+        await category.destroy().catch((error) => {
+            throw new AppError("Erro interno do servidor!", 500, error);
+        });
+        return category;
     }
 }
 
