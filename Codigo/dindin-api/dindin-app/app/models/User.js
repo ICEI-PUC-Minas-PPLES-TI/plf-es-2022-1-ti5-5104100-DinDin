b9c4b86ccabde1aebc7bb714be8e46f595a1aa3e
Lucas Angelo O M Rocha <lcs2001_@hotmail.com>
2022-04-24 23:07:05 -0300

    Fix lint

--- Codigo/dindin-api/dindin-app/app/models/User.js
+++ Codigo/dindin-api/dindin-app/app/models/User.js
@@ -1,106 +1,105 @@
 const { Model, DataTypes } = require("sequelize");
 
 class User extends Model {
-  static init(sequelize) {
-    super.init(
-      {
-        id: {
-          field: 'id',
-          type: DataTypes.INTEGER(11).UNSIGNED,
-          autoIncrement: true,
-          primaryKey: true,
-          allowNull: false,
-          required: true,
-          notEmpty: true,
-          validate: {
-            notEmpty: true,
-          }
-        },
-        name: {
-          field: 'name',
-          type: DataTypes.STRING(100),
-          notEmpty: true,
-          allowNull: false,
-          notEmpty: true,
-          validate: {
-            notEmpty: true,
-          }
-        },
-        email: {
-          field: 'email',
-          type: DataTypes.STRING(150),
-          unique: true,
-          allowNull: false,
-          notEmpty: true,
-          validate: {
-            notEmpty: true,
-            isEmail: true
-          }
-        },
-        password: {
-          field: 'password',
-          type: DataTypes.STRING(64),
-          notEmpty: true,
-          allowNull: true,
-          validate: {
-            notEmpty: true,
-          },
-          comment: 'Encrypted with 64 digits'
-        },
-        firebaseId: {
-          field: 'firebase_id',
-          type: DataTypes.STRING(128),
-          notEmpty: true,
-          unique: true,
-          allowNull: true,
-          validate: {
-            notEmpty: true,
-          }
-        },
-        created_at: {
-          type: DataTypes.DATE,
-          allowNull: false,
-        },
-        updated_at: {
-          type: DataTypes.DATE,
-          allowNull: false,
-        },
-        deleted_at: {
-          type: DataTypes.DATE,
-          allowNull: true,
-          defaultValue: null,
-        }
-      },
-      {
-        tableName: "user",
-        charset: 'utf8mb4',
-        collate: 'utf8mb4_bin',
-        timestamps: true, // deleted_at and updatedAt need this
-        paranoid: true, // deleted_at need this
-        createdAt: "created_at",
-        updatedAt: "updated_at",
-        deletedAt: "deleted_at", // .destroy() and .destroy(); to softdelete
-        sequelize,
-        defaultScope: {
-          attributes: {
-            exclude: [
-              "password" // To not return password
-            ]
-          }
-        },
-        scopes: {
-          withPassword: {
-            attributes: {},
-          },
-          deleted: {
-            where: {
-              deleted: true
+    static init(sequelize) {
+        super.init(
+            {
+                id: {
+                    field: "id",
+                    type: DataTypes.INTEGER(11).UNSIGNED,
+                    autoIncrement: true,
+                    primaryKey: true,
+                    allowNull: false,
+                    required: true,
+                    notEmpty: true,
+                    validate: {
+                        notEmpty: true,
+                    },
+                },
+                name: {
+                    field: "name",
+                    type: DataTypes.STRING(100),
+                    notEmpty: true,
+                    allowNull: false,
+                    validate: {
+                        notEmpty: true,
+                    },
+                },
+                email: {
+                    field: "email",
+                    type: DataTypes.STRING(150),
+                    unique: true,
+                    allowNull: false,
+                    notEmpty: true,
+                    validate: {
+                        notEmpty: true,
+                        isEmail: true,
+                    },
+                },
+                password: {
+                    field: "password",
+                    type: DataTypes.STRING(64),
+                    notEmpty: true,
+                    allowNull: true,
+                    validate: {
+                        notEmpty: true,
+                    },
+                    comment: "Encrypted with 64 digits",
+                },
+                firebaseId: {
+                    field: "firebase_id",
+                    type: DataTypes.STRING(128),
+                    notEmpty: true,
+                    unique: true,
+                    allowNull: true,
+                    validate: {
+                        notEmpty: true,
+                    },
+                },
+                created_at: {
+                    type: DataTypes.DATE,
+                    allowNull: false,
+                },
+                updated_at: {
+                    type: DataTypes.DATE,
+                    allowNull: false,
+                },
+                deleted_at: {
+                    type: DataTypes.DATE,
+                    allowNull: true,
+                    defaultValue: null,
+                },
+            },
+            {
+                tableName: "user",
+                charset: "utf8mb4",
+                collate: "utf8mb4_bin",
+                timestamps: true, // deleted_at and updatedAt need this
+                paranoid: true, // deleted_at need this
+                createdAt: "created_at",
+                updatedAt: "updated_at",
+                deletedAt: "deleted_at", // .destroy() and .destroy(); to softdelete
+                sequelize,
+                defaultScope: {
+                    attributes: {
+                        exclude: [
+                            "password", // To not return password
+                        ],
+                    },
+                },
+                scopes: {
+                    withPassword: {
+                        attributes: {},
+                    },
+                    deleted: {
+                        where: {
+                            deleted: true,
+                        },
+                    },
+                },
             }
-          },
-        }
-      }
-    );
-  }
+        );
+    }
 }
 
 module.exports = User;
--- Codigo/dindin-api/dindin-app/app/usecases/user/authenticate/AuthenticateUserAccountUseCase.js
+++ Codigo/dindin-api/dindin-app/app/usecases/user/authenticate/AuthenticateUserAccountUseCase.js
@@ -6,68 +6,73 @@ const AppError = require("../../../errors/AppError");
 const User = require("../../../models/User");
 
 class AuthenticateUserAccountUseCase {
+    // ver como vai ser estruturado isso, se vai ser necessário
+    // criar outro arquivo ou algo do tipo
+    async executeForLocalAuth(email, password) {
+        const user = await User.findOne({
+            where: { email },
+            attributes: {
+                include: "password",
+            },
+        });
 
-  // ver como vai ser estruturado isso, se vai ser necessário
-  // criar outro arquivo ou algo do tipo
-  async executeForLocalAuth(email, password) {
-    const user = await User.findOne({
-      where: { email },
-      attributes: {
-        include: "password"
-      }
-    })
+        if (!user)
+            throw new AppError(
+                "Não existe usuário com esse e-mail cadastrado o sistema",
+                401
+            );
 
-    if (!user)
-      throw new AppError("Não existe usuário com esse e-mail cadastrado o sistema", 401);
+        const arePasswordsEqual = await bcrypt.compare(password, user.password);
+        if (!arePasswordsEqual) throw new AppError("Senha incorreta!", 401);
 
-    const arePasswordsEqual = await bcrypt.compare(password, user.password);
-    if (!arePasswordsEqual)
-      throw new AppError("Senha incorreta!", 401);
+        // gerar jwt
+        const jwt = jwtAuthorization.logIn(user.id);
+        const firebaseToken = await firebaseServices.generateFirebaseAuthToken(
+            user.id
+        );
 
-    // gerar jwt
-    const jwt = jwtAuthorization.logIn(user.id);
-    const firebaseToken = await firebaseServices.generateFirebaseAuthToken(user.id);
-
-    return {
-      jwt,
-      firebaseToken
-    }
-  }
-
-  async executeForFirebaseAuth(firebaseToken) {
-    let firebaseId;
-    try {
-      firebaseId = await firebaseServices.getFirebaseIdByToken(firebaseToken);
-    } catch (error) {
-      throw new AppError("Não foi possível fazer login com entidade federada", 401);
+        return {
+            jwt,
+            firebaseToken,
+        };
     }
 
-    let user = await User.findOne({
-      where: { firebaseId },
-      attributes: {
-        include: "id"
-      }
-    })
+    async executeForFirebaseAuth(firebaseToken) {
+        let firebaseId;
+        try {
+            firebaseId = await firebaseServices.getFirebaseIdByToken(
+                firebaseToken
+            );
+        } catch (error) {
+            throw new AppError(
+                "Não foi possível fazer login com entidade federada",
+                401
+            );
+        }
 
-    if (!user){
-      const {
-        email,
-        displayName: name
-      } = await firebaseServices.getFirebaseUserById(firebaseId);
+        let user = await User.findOne({
+            where: { firebaseId },
+            attributes: {
+                include: "id",
+            },
+        });
 
-      user = await User.create({
-        name,
-        email,
-        firebaseId
-      }).catch(error => {
-        throw new AppError(error.message, 500, error);
-      });
+        if (!user) {
+            const { email, displayName: name } =
+                await firebaseServices.getFirebaseUserById(firebaseId);
+
+            user = await User.create({
+                name,
+                email,
+                firebaseId,
+            }).catch((error) => {
+                throw new AppError(error.message, 500, error);
+            });
+        }
+
+        // gerar jwt
+        return jwtAuthorization.logIn(user.id);
     }
-
-    // gerar jwt
-    return jwtAuthorization.logIn(user.id);
-  }
-
 }
 
 module.exports = AuthenticateUserAccountUseCase;
