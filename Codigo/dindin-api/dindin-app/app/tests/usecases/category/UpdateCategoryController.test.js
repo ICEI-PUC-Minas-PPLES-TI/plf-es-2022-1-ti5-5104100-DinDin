41760153157cf010a610cd5e8bcf2da09f767c29
Lucas Angelo O M Rocha <lcs2001_@hotmail.com>
2022-04-25 16:57:59 -0300

    Improve update category test suite

--- Codigo/dindin-api/dindin-app/app/tests/usecases/category/UpdateCategoryController.test.js
+++ Codigo/dindin-api/dindin-app/app/tests/usecases/category/UpdateCategoryController.test.js
@@ -31,8 +31,61 @@ describe("PUT /category test suite", () => {
             .put("/api/Category/" + createdCategory.id)
             .send(mockCategory);
 
-        expect(response.statusCode).toEqual(201);
+        expect(response.statusCode).toEqual(200);
         expect(response.body.description).toEqual(mockCategory.description);
         expect(response.body.color).toEqual(mockCategory.color);
+    });
+
+    it("should fail validation with big description", async () => {
+        const response = await supertest(app).put("/api/category/1").send({
+            description: "MaisDe30CaracteresParaRetornar422",
+            type: "OUT",
+            color: "FF0000",
+        });
+        expect(response.statusCode).toEqual(422);
+    });
+
+    it("should not update a new category with invalid type", async () => {
+        const response = await supertest(app).put("/api/category/1").send({
+            description: "test",
+            type: "invalid",
+            color: "FF0000",
+        });
+        expect(response.statusCode).toEqual(422);
+    });
+
+    it("should not update a new category with invalid color (small)", async () => {
+        const response = await supertest(app).put("/api/category/1").send({
+            description: "test",
+            type: "OUT",
+            color: "so3",
+        });
+        expect(response.statusCode).toEqual(422);
+    });
+
+    it("should not update a new category with invalid color (big)", async () => {
+        const response = await supertest(app).put("/api/category/1").send({
+            description: "test",
+            type: "OUT",
+            color: "maisde6",
+        });
+        expect(response.statusCode).toEqual(422);
+    });
+
+    it("should not category wallet_id and user_id", async () => {
+        const category = await Category.findByPk(1);
+        const oldWalletId = category.wallet_id;
+        const oldUserId = category.wallet_id;
+        const response = await supertest(app).put("/api/category/1").send({
+            user_id: "24898489",
+            wallet_id: "24898489",
+        });
+        expect(response.statusCode).toEqual(200);
+        expect(response.body).toHaveProperty("id");
+        expect(response.body).toHaveProperty("user_id");
+        expect(response.body).toHaveProperty("wallet_id");
+        const getResponse = await supertest(app).get("/api/category/1");
+        expect(getResponse.body.wallet_id).toEqual(oldUserId);
+        expect(getResponse.body.wallet_id).toEqual(oldWalletId);
     });
 });
