27fcb1ca625c2d8c220e7988d3bc8258e83fb3aa
Guilherme Gabriel <guilherme.gabriel.532@gmail.com>
2022-04-25 21:23:23 -0300

    changed firebase services from classes, cleaning lint warning

--- Codigo/dindin-api/dindin-app/app/services/firebaseServices.js
+++ Codigo/dindin-api/dindin-app/app/services/firebaseServices.js
@@ -14,40 +14,32 @@ const initializeApp = () => firebaseAdmin.initializeApp({
 
 const getAuth = firebaseAdmin.auth;
 
-class firebaseServicesSingleton{
-    static instance = new firebaseServicesSingleton();
+let firebaseApp = null;
 
-    firebaseApp = null;
+function getFirebaseInstance(){
 
-    constructor(){
-        this.firebaseApp = initializeApp();
-        
-    }
+    if (!firebaseApp)
+        firebaseApp = initializeApp();
 
-    static getFirebaseInstance(){
-        if (this.instance === null)
-            this.instance = new firebaseServicesSingleton()
-        // coloquei retornando assim só pra ele tipar automaticamnete
-        return this.instance ?? new firebaseServicesSingleton() ;
-    }
-
-    async generateFirebaseAuthToken(idUsuario){
-        return getAuth().createCustomToken(`uid@${idUsuario}`)
-    }
-
-    async getFirebaseIdByToken(token){
-        const verifyIdTokenResult = await getAuth().verifyIdToken(token);
-        return verifyIdTokenResult.uid;
-    }
-
-    async getFirebaseUserById(uid){
-        return getAuth().getUser(uid);
+    return {
+        async generateFirebaseAuthToken(idUsuario){
+            return getAuth().createCustomToken(`uid@${idUsuario}`)
+        },
+    
+        async getFirebaseIdByToken(token){
+            const verifyIdTokenResult = await getAuth().verifyIdToken(token);
+            return verifyIdTokenResult.uid;
+        },
+    
+        async getFirebaseUserById(uid){
+            return getAuth().getUser(uid);
+        }
     }
 
 }
 
 module.exports = {
     get firebaseServices() { 
-        return firebaseServicesSingleton.getFirebaseInstance()
+        return getFirebaseInstance()
     }
 }
