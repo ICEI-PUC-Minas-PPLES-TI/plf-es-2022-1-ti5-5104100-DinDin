62e660d1bf44ad0510855ccc601dfb2a63233b70
Guilherme Gabriel <guilherme.gabriel.532@gmail.com>
2022-04-25 21:30:42 -0300

    lint fix

--- Codigo/dindin-api/dindin-app/app/services/firebaseServices.js
+++ Codigo/dindin-api/dindin-app/app/services/firebaseServices.js
@@ -1,45 +1,44 @@
-'use strict';
+"use strict";
 
 const firebaseAdmin = require("firebase-admin");
 
 const firebaseCredentials = {
-  projectId: "dindinfinance",
-  clientEmail: "firebase-adminsdk-xmgz0@dindinfinance.iam.gserviceaccount.com",
-  privateKey: process.env.FB_PRIVATE_KEY
+    projectId: "dindinfinance",
+    clientEmail:
+        "firebase-adminsdk-xmgz0@dindinfinance.iam.gserviceaccount.com",
+    privateKey: process.env.FB_PRIVATE_KEY,
 };
 
-const initializeApp = () => firebaseAdmin.initializeApp({
-    credential: firebaseAdmin.credential.cert(firebaseCredentials),
-});
+const initializeApp = () =>
+    firebaseAdmin.initializeApp({
+        credential: firebaseAdmin.credential.cert(firebaseCredentials),
+    });
 
 const getAuth = firebaseAdmin.auth;
 
 let firebaseApp = null;
 
-function getFirebaseInstance(){
-
-    if (!firebaseApp)
-        firebaseApp = initializeApp();
+function getFirebaseInstance() {
+    if (!firebaseApp) firebaseApp = initializeApp();
 
     return {
-        async generateFirebaseAuthToken(idUsuario){
-            return getAuth().createCustomToken(`uid@${idUsuario}`)
+        async generateFirebaseAuthToken(idUsuario) {
+            return getAuth().createCustomToken(`uid@${idUsuario}`);
         },
-    
-        async getFirebaseIdByToken(token){
+
+        async getFirebaseIdByToken(token) {
             const verifyIdTokenResult = await getAuth().verifyIdToken(token);
             return verifyIdTokenResult.uid;
         },
-    
-        async getFirebaseUserById(uid){
-            return getAuth().getUser(uid);
-        }
-    }
 
+        async getFirebaseUserById(uid) {
+            return getAuth().getUser(uid);
+        },
+    };
 }
 
 module.exports = {
-    get firebaseServices() { 
-        return getFirebaseInstance()
-    }
-}
+    get firebaseServices() {
+        return getFirebaseInstance();
+    },
+};
