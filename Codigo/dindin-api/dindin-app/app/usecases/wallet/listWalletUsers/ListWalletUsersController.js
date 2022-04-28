da580c6f9e81a3fa4296cd360b4b22feab8d2449
Lucas Angelo O M Rocha <lcs2001_@hotmail.com>
2022-04-28 10:42:37 -0300

    Check if id exist

--- Codigo/dindin-api/dindin-app/app/usecases/wallet/listWalletUsers/ListWalletUsersController.js
+++ Codigo/dindin-api/dindin-app/app/usecases/wallet/listWalletUsers/ListWalletUsersController.js
@@ -1,7 +1,20 @@
+const yup = require("yup");
+
 const ListWalletUsersUseCase = require("./ListWalletUsersUseCase");
+const AppError = require("../../../errors/AppError");
 
 class ListWalletUsersController {
     async listUsers(request, response) {
+        const scheme = yup.object().shape({
+            id: yup.number("'id' must be numeric!").min(1).required(),
+        });
+
+        try {
+            await scheme.validate(request.params, { abortEarly: false });
+        } catch (error) {
+            throw new AppError(error.name, 422, error.errors);
+        }
+
         const { id } = request.params;
 
         const listWalletUsersUseCase = new ListWalletUsersUseCase();
