939e5c30c98ea73d8bef868041129062504c364d
Vinicius Marini Costa E Oliveira <475997@sga.pucminas.br>
2022-05-04 20:26:22 -0300

    get data from input

--- Codigo/dindin-mobile/dindin/lib/pages/wallet/create.dart
+++ Codigo/dindin-mobile/dindin/lib/pages/wallet/create.dart
@@ -11,11 +11,16 @@ class WalletCreate extends StatefulWidget {
 
 class _WalletCreateState extends State<WalletCreate> {
   bool _isShared = false;
+  final description = TextEditingController();
+  final startingAmount = TextEditingController();
+  final inviteCode = TextEditingController();
+
   void changeButtonState() {
     setState(() {
       _isShared = !_isShared;
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,12 +38,36 @@ class _WalletCreateState extends State<WalletCreate> {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          SizedBox(
-                            height: MediaQuery.of(context).size.height * 0.1,
-                            width: MediaQuery.of(context).size.width * 0.4,
-                            child: ElevatedButton(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.1,
+                          width: MediaQuery.of(context).size.width * 0.4,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              changeButtonState();
+                            },
+                            style: _isShared
+                                ? ButtonStyle(
+                                    backgroundColor:
+                                        MaterialStateProperty.all<Color>(
+                                    Colors.grey,
+                                  ))
+                                : ButtonStyle(
+                                    backgroundColor:
+                                        MaterialStateProperty.all<Color>(
+                                            Theme.of(context).primaryColor),
+                                  ),
+                            child: const Text(
+                              "Create new",
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.1,
+                          width: MediaQuery.of(context).size.width * 0.4,
+                          child: ElevatedButton(
                               onPressed: () {
                                 changeButtonState();
                               },
@@ -46,78 +75,63 @@ class _WalletCreateState extends State<WalletCreate> {
                                   ? ButtonStyle(
                                       backgroundColor:
                                           MaterialStateProperty.all<Color>(
-                                      Colors.grey,
-                                    ))
+                                              Theme.of(context).primaryColor),
+                                    )
                                   : ButtonStyle(
                                       backgroundColor:
                                           MaterialStateProperty.all<Color>(
-                                              Theme.of(context).primaryColor),
+                                              Colors.grey),
                                     ),
-                              child: const Text(
-                                "Create new",
-                                style: TextStyle(fontSize: 20),
-                              ),
-                            ),
-                          ),
-                          SizedBox(
-                            height: MediaQuery.of(context).size.height * 0.1,
-                            width: MediaQuery.of(context).size.width * 0.4,
-                            child: ElevatedButton(
-                                onPressed: () {
-                                  changeButtonState();
-                                },
-                                style: _isShared
-                                    ? ButtonStyle(
-                                        backgroundColor:
-                                            MaterialStateProperty.all<Color>(
-                                                Theme.of(context).primaryColor),
-                                      )
-                                    : ButtonStyle(
-                                        backgroundColor:
-                                            MaterialStateProperty.all<Color>(
-                                                Colors.grey),
-                                      ),
-                                child: const Text("Join",
-                                    style: TextStyle(fontSize: 20))),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(height: 20),
+                              child: const Text("Join",
+                                  style: TextStyle(fontSize: 20))),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
                     //only visible if shared
-                    if(_isShared) Text('Wallet invite code',
-                        style: TextStyle(fontWeight: FontWeight.bold)),
-                    if(_isShared) const TextField(
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          hintText: 'ex: AAA323#',
-                          suffixIcon:
-                              Icon(FontAwesomeIcons.penToSquare, size: 20.0)),
-                    ),
-                    if(_isShared) const SizedBox(height: 20),
+                    if (_isShared)
+                      const Text('Wallet invite code',
+                          style: TextStyle(fontWeight: FontWeight.bold)),
+                    if (_isShared)
+                      TextField(
+                        controller: inviteCode,
+                        decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'ex: AAA323#',
+                            suffixIcon:
+                                Icon(FontAwesomeIcons.penToSquare, size: 20.0)),
+                      ),
+                    if (_isShared) const SizedBox(height: 20),
                     // Description field
-                    if(!_isShared) const Text('Description',
-                        style: TextStyle(fontWeight: FontWeight.bold)),
-                    if(!_isShared) const TextField(
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          hintText: 'ex: Money under the mattress',
-                          suffixIcon:
-                              Icon(FontAwesomeIcons.penToSquare, size: 20.0)),
-                    ),
+                    if (!_isShared)
+                      const Text('Description',
+                          style: TextStyle(fontWeight: FontWeight.bold)),
+                    if (!_isShared)
+                      TextField(
+                        controller: description,
+                        decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'ex: Money under the mattress',
+                            suffixIcon:
+                                Icon(FontAwesomeIcons.penToSquare, size: 20.0)),
+                      ),
                     // End description dield
-                    if(!_isShared) const SizedBox(height: 20),
+                    if (!_isShared) const SizedBox(height: 20),
                     // Numeric input Starting Amount
-                    if(!_isShared) const Text('Starting Amount',
-                        style: TextStyle(fontWeight: FontWeight.bold)),
-                    if(!_isShared) const TextField(
-                      keyboardType: TextInputType.number,
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          hintText: 'ex: 9999,99',
-                          suffixIcon:
-                              Icon(FontAwesomeIcons.penToSquare, size: 20.0)),
-                    ),
-                    if(!_isShared) const SizedBox(height: 20),
+                    if (!_isShared)
+                      const Text('Starting Amount',
+                          style: TextStyle(fontWeight: FontWeight.bold)),
+                    if (!_isShared)
+                      TextField(
+                        controller: startingAmount,
+                        keyboardType: TextInputType.number,
+                        decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'ex: 9999,99',
+                            suffixIcon:
+                                Icon(FontAwesomeIcons.penToSquare, size: 20.0)),
+                      ),
+                    if (!_isShared) const SizedBox(height: 20),
                     SizedBox(
                       height: 40,
                       width: double.infinity,
@@ -126,7 +140,16 @@ class _WalletCreateState extends State<WalletCreate> {
                         style: ElevatedButton.styleFrom(
                           primary: Theme.of(context).primaryColor,
                         ),
-                        onPressed: () {},
+                        onPressed: () {
+                          if(_isShared){
+                            print("Joining wallet");
+                            print(inviteCode.text);
+                          }else{
+                            print("Creating new wallet");
+                            print(description.text);
+                            print(startingAmount.text);
+                          }
+                        },
                       ),
                     )
                     // End wallet list
