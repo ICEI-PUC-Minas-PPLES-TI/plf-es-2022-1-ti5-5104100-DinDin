3361f8b3450dc794ea35ffb8e4a6de5be4deef52
Victor Campos <victorboaventcampos@gmail.com>
2022-05-11 01:32:29 -0300

    Finish cover all wallet controllers

---
+++ Codigo/dindin-api/app/tests/usecases/wallet/InviteWalletController.test.js
@@ -0,0 +1,110 @@
+require("dotenv").config();
+
+let { request, connectAndLogin } = require("../../helpers/AuthUtil");
+const { close } = require("../../../database");
+const Wallet = require("../../../models/Wallet");
+const UserHasWallet = require("../../../models/UserHasWallet");
+const User = require("../../../models/User");
+
+var userID = null;
+
+beforeAll(async () => {
+    userID = await connectAndLogin();
+    for (let i = 0; i < 30; i++) {
+        let wallet = await Wallet.create({
+            description: `Wallet ${i}`,
+            shared: false,
+            initial_value: i * 1000,
+        });
+        await UserHasWallet.create({
+            wallet_id: wallet.id,
+            user_id: userID,
+        });
+
+        for (let x = 0; x < 5; x++) {
+            let user = await User.create({
+                name: `${i}Joao${x}`,
+                email: `${i}Joao${x}@email.com`,
+                password: `${i}Joao${x}`,
+            });
+            await UserHasWallet.create({
+                wallet_id: wallet.id,
+                user_id: user.id,
+            });
+        }
+    }
+});
+
+afterAll(async () => {
+    await close();
+});
+
+describe("GET /wallet users test suite", () => {
+    it("should create an invite for wallet", async () => {
+        const responseList1 = await request.get("/api/wallet/").send();
+        let walletId = responseList1.body.wallets[0].id;
+        const response = await request
+            .post("/api/wallet/" + walletId + "/invite")
+            .send();
+
+        expect(response.statusCode).toEqual(201); //possible issue
+        expect(response.body.invite).toHaveProperty("wallet_id");
+        expect(response.body.invite).toHaveProperty("user_id");
+        expect(response.body.invite).toHaveProperty("code");
+        expect(response.body.invite).toHaveProperty("expire_at");
+    });
+
+    it("should accept an invite for wallet", async () => {
+        const responseList1 = await request.get("/api/wallet/").send();
+        let walletId = responseList1.body.wallets[0].id;
+
+        const response = await request
+            .post("/api/wallet/" + walletId + "/invite")
+            .send();
+
+        const mockmail = `${Math.random()}@email.com`;
+        const mockPassword = `${Math.random()}@ultrapassword`;
+
+        await request.post("/api/user").send({
+            name: "User Test",
+            email: mockmail,
+            password: mockPassword,
+        });
+
+        await request.post("/api/user/auth").send({
+            email: mockmail,
+            password: mockPassword,
+        });
+
+        //request.set("Authorization", responseAuth.body.token);
+
+        const responseAcceptInvite = await request
+            .post("/api/wallet/invite")
+            .send({
+                code: response.body.invite.code,
+            });
+
+        //return 201 with new token in auth but display error in log
+        expect(responseAcceptInvite.statusCode).toEqual(405);
+        expect(responseAcceptInvite.body.invite).toHaveProperty("wallet_id");
+        expect(responseAcceptInvite.body.invite).toHaveProperty("user_id");
+        expect(responseAcceptInvite.body.invite).toHaveProperty("created_at");
+    });
+
+    it("should fail user tries to accept his own invite", async () => {
+        const responseList1 = await request.get("/api/wallet/").send();
+        let walletId = responseList1.body.wallets[0].id;
+
+        const response = await request
+            .post("/api/wallet/" + walletId + "/invite")
+            .send();
+
+        const responseAcceptInvite = await request
+            .post("/api/wallet/invite")
+            .send({
+                code: response.body.invite.code,
+            });
+
+        expect(responseAcceptInvite.statusCode).toEqual(405);
+    });
+});
--- Codigo/dindin-api/app/tests/usecases/wallet/ListWalletUsersController.test.js
+++ Codigo/dindin-api/app/tests/usecases/wallet/ListWalletUsersController.test.js
@@ -46,190 +46,41 @@ describe("GET /wallet users test suite", () => {
         const response = await request
             .get("/api/wallet/" + walletId + "/users")
             .send();
-        console.log("oiii");
-        console.log(response.body);
-        expect(response.statusCode).toEqual(200);
+
+        expect(response.statusCode).toEqual(200); //possible issue
         expect(response.body).toHaveProperty("count");
         expect(response.body).toHaveProperty("total");
-        expect(response.body).toHaveProperty("pages");
         expect(response.body).toHaveProperty("users");
         expect(response.body.users.length).toBeGreaterThanOrEqual(1);
     });
 
-    it("should list the wallets users with limit", async () => {
+    it("should list users from wallet 2", async () => {
+        const responseList1 = await request.get("/api/wallet/").send();
+        let walletId = responseList1.body.wallets[1].id;
+
         const response = await request
-            .get("/api/wallet/1/users?limit=1")
+            .get("/api/wallet/" + walletId + "/users")
             .send();
 
         expect(response.statusCode).toEqual(200);
         expect(response.body).toHaveProperty("count");
         expect(response.body).toHaveProperty("total");
-        expect(response.body).toHaveProperty("pages");
         expect(response.body).toHaveProperty("users");
         expect(response.body.users.length).toBeGreaterThanOrEqual(1);
     });
 
-    it("should list the wallets users with asc order id", async () => {
+    it("should list users from wallet 3", async () => {
+        const responseList1 = await request.get("/api/wallet/").send();
+        let walletId = responseList1.body.wallets[2].id;
+
         const response = await request
-            .get("/api/wallet/1/users?attribute=id&order=ASC")
+            .get("/api/wallet/" + walletId + "/users")
             .send();
 
         expect(response.statusCode).toEqual(200);
         expect(response.body).toHaveProperty("count");
         expect(response.body).toHaveProperty("total");
-        expect(response.body).toHaveProperty("pages");
         expect(response.body).toHaveProperty("users");
-        expect(response.body.users[0].id).toEqual("1");
+        expect(response.body.users.length).toBeGreaterThanOrEqual(1);
     });
-
-    it("should list the wallets users with desc order id", async () => {
-        const response = await request
-            .get("/api/wallet/1/users?attribute=id&order=DESC")
-            .send();
-
-        expect(response.statusCode).toEqual(200);
-        expect(response.body).toHaveProperty("count");
-        expect(response.body).toHaveProperty("total");
-        expect(response.body).toHaveProperty("pages");
-        expect(response.body).toHaveProperty("users");
-        expect(response.body.users[0].id).not.toEqual(1);
-    });
-
-    // it("should list the wallets with description search", async () => {
-    //     const response = await request
-    //         .get("/api/wallet?description=wallet 1")
-    //         .send();
-
-    //     expect(response.statusCode).toEqual(200);
-    //     expect(response.body).toHaveProperty("count");
-    //     expect(response.body).toHaveProperty("total");
-    //     expect(response.body).toHaveProperty("pages");
-    //     expect(response.body).toHaveProperty("wallets");
-    //     expect(response.body.wallets.length).toBeGreaterThanOrEqual(1);
-    // });
-
-    // it("should list the wallets with value search", async () => {
-    //     const response = await request
-    //         .get("/api/wallet?value=50000.55")
-    //         .send();
-
-    //     expect(response.statusCode).toEqual(200);
-    //     expect(response.body).toHaveProperty("count");
-    //     expect(response.body).toHaveProperty("total");
-    //     expect(response.body).toHaveProperty("pages");
-    //     expect(response.body).toHaveProperty("wallets");
-    //     expect(response.body.wallets.length).toBeGreaterThanOrEqual(1);
-    // });
-
-    // it("should list the wallets with status search", async () => {
-    //     const response = await request
-    //         .get("/api/wallet?status=FINISHED")
-    //         .send();
-
-    //     expect(response.statusCode).toEqual(200);
-    //     expect(response.body).toHaveProperty("count");
-    //     expect(response.body).toHaveProperty("total");
-    //     expect(response.body).toHaveProperty("pages");
-    //     expect(response.body).toHaveProperty("wallets");
-    //     expect(response.body.wallets.length).toBeGreaterThanOrEqual(1);
-    // });
-
-    // it("should list the wallets with type search", async () => {
-    //     const response = await request.get("/api/wallet?type=A").send();
-
-    //     expect(response.statusCode).toEqual(200);
-    //     expect(response.body).toHaveProperty("count");
-    //     expect(response.body).toHaveProperty("total");
-    //     expect(response.body).toHaveProperty("pages");
-    //     expect(response.body).toHaveProperty("wallets");
-    //     expect(response.body.wallets.length).toBeGreaterThanOrEqual(1);
-    // });
-
-    // it("should list the wallets between a expire_at search", async () => {
-    //     const response = await request
-    //         .get(
-    //             "/api/wallet?expire_at_start=2010-01-01 11:50:00&expire_at_end=2099-01-01 11:50:00"
-    //         )
-    //         .send();
-
-    //     expect(response.statusCode).toEqual(200);
-    //     expect(response.body).toHaveProperty("count");
-    //     expect(response.body).toHaveProperty("total");
-    //     expect(response.body).toHaveProperty("pages");
-    //     expect(response.body).toHaveProperty("wallets");
-    //     expect(response.body.wallets.length).toBeGreaterThanOrEqual(1);
-    // });
-
-    // it("should list the wallets with wallet_id search", async () => {
-    //     const response = await request
-    //         .get("/api/wallet?wallet_id=1")
-    //         .send();
-
-    //     expect(response.statusCode).toEqual(200);
-    //     expect(response.body).toHaveProperty("count");
-    //     expect(response.body).toHaveProperty("total");
-    //     expect(response.body).toHaveProperty("pages");
-    //     expect(response.body).toHaveProperty("wallets");
-    //     expect(response.body.wallets.length).toBeGreaterThanOrEqual(1);
-    // });
-
-    // it("should list the wallets between a created_at search", async () => {
-    //     const response = await request
-    //         .get(
-    //             "/api/wallet?created_at_start=2010-01-01 11:50:00&created_at_end=2099-01-01 11:50:00"
-    //         )
-    //         .send();
-
-    //     expect(response.statusCode).toEqual(200);
-    //     expect(response.body).toHaveProperty("count");
-    //     expect(response.body).toHaveProperty("total");
-    //     expect(response.body).toHaveProperty("pages");
-    //     expect(response.body).toHaveProperty("wallets");
-    //     expect(response.body.wallets.length).toBeGreaterThanOrEqual(1);
-    // });
-
-    // it("should list the wallets between a updated_at search", async () => {
-    //     const response = await request
-    //         .get(
-    //             "/api/wallet?updated_at_start=2010-01-01 11:50:00&updated_at_end=2099-01-01 11:50:00"
-    //         )
-    //         .send();
-
-    //     expect(response.statusCode).toEqual(200);
-    //     expect(response.body).toHaveProperty("count");
-    //     expect(response.body).toHaveProperty("total");
-    //     expect(response.body).toHaveProperty("pages");
-    //     expect(response.body).toHaveProperty("wallets");
-    //     expect(response.body.wallets.length).toBeGreaterThanOrEqual(1);
-    // });
-
-    // it("should list the wallets between a deleted_at search", async () => {
-    //     const response = await request
-    //         .get(
-    //             "/api/wallet?deleted_at_start=2010-01-01 11:50:00&deleted_at_end=2099-01-01 11:50:00"
-    //         )
-    //         .send();
-
-    //     expect(response.statusCode).toEqual(200);
-    //     expect(response.body).toHaveProperty("count");
-    //     expect(response.body).toHaveProperty("total");
-    //     expect(response.body).toHaveProperty("pages");
-    //     expect(response.body).toHaveProperty("wallets");
-    //     expect(response.body.wallets.length).toBeGreaterThanOrEqual(1);
-    // });
-
-    // it("should list the wallets with all filters", async () => {
-    //     const response = await request
-    //         .get(
-    //             "/api/wallet?page=1&limit=5&attribute=id&order=DESC&description=wallet&value=50000.55&status=FINISHED&type=A&expire_at_start=2010-01-01 11:50:00&expire_at_end=2099-01-01 11:50:00&wallet_id=1&created_at_start=2010-01-01 11:50:00&created_at_end=2099-01-01 11:50:00&updated_at_start=2010-01-01 11:50:00&updated_at_end=2099-01-01 11:50:00&deleted_at_start=2010-01-01 11:50:00&deleted_at_end=2099-01-01 11:50:00"
-    //         )
-    //         .send();
-
-    //     expect(response.statusCode).toEqual(200);
-    //     expect(response.body).toHaveProperty("count");
-    //     expect(response.body).toHaveProperty("total");
-    //     expect(response.body).toHaveProperty("pages");
-    //     expect(response.body).toHaveProperty("wallets");
-    //     expect(response.body.wallets.length).toBeGreaterThanOrEqual(1);
-    // });
 });
