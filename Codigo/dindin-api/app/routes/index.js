5fc22def78ec0e709703edf8a4189cc61b06c07c
Lucas Angelo O M Rocha <lcs2001_@hotmail.com>
2022-05-18 00:51:58 -0300

    Fix invalid route

--- Codigo/dindin-api/app/routes/index.js
+++ Codigo/dindin-api/app/routes/index.js
@@ -5,13 +5,11 @@ const userRoutes = require("./userRoutes");
 const goalRoutes = require("./goalRoutes");
 const walletRoutes = require("./walletRoutes");
 const walletCategoryRoutes = require("./walletCategoryRoutes");
-const categoryRoutes = require("./categoryRoutes");
 const transactionRoutes = require("./transactionRoutes");
 const transactionRecurrenciesRoutes = require("./transactionRecurrenciesRoutes");
 
 router.use("/user", userRoutes);
 router.use("/goal", goalRoutes);
-router.use("/category", categoryRoutes);
 router.use("/wallet", walletRoutes);
 router.use(walletCategoryRoutes);
 router.use("/transaction", transactionRoutes);
