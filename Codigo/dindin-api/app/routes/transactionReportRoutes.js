968eec5e3b5d4703a471db31cdca87aaf2e8f71e
Lucas Angelo O M Rocha <lcs2001_@hotmail.com>
2022-05-18 00:40:25 -0300

    Remove middleware

--- Codigo/dindin-api/app/routes/transactionReportRoutes.js
+++ Codigo/dindin-api/app/routes/transactionReportRoutes.js
@@ -3,18 +3,13 @@ const transactionReportRoutes = Router();
 
 const AuthenticationMiddleware = require("../middleware/AuthenticationMiddleware");
 
-const CategoryBelongsUserMiddleware = require("../middleware/CategoryBelongsUserMiddleware");
-
 const ReportUserTotalTransactionController = require("../usecases/transaction/reportUserTotalTransaction/ReportUserTotalTransactionController");
 const reportUserTotalTransactionController =
     new ReportUserTotalTransactionController();
 
 transactionReportRoutes.get(
     "/usertotal",
-    [
-        AuthenticationMiddleware.verifyToken,
-        CategoryBelongsUserMiddleware.verifyCategoryBelongsUser,
-    ],
+    [AuthenticationMiddleware.verifyToken],
     reportUserTotalTransactionController.report
 );
 
