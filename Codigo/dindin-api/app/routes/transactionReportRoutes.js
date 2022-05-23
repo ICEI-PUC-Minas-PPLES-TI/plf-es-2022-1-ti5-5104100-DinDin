afcc2648d479abc7a9bbb6a88a79e5c5f95a7443
Lucas Angelo O M Rocha <lcs2001_@hotmail.com>
2022-05-23 01:28:05 -0300

    Report Category route

--- Codigo/dindin-api/app/routes/transactionReportRoutes.js
+++ Codigo/dindin-api/app/routes/transactionReportRoutes.js
@@ -6,6 +6,7 @@ const AuthenticationMiddleware = require("../middleware/AuthenticationMiddleware
 const ReportUserTotalTransactionController = require("../usecases/transaction/reportUserTotalTransaction/ReportUserTotalTransactionController");
 const ReportWalletBalanceTransactionController = require("../usecases/transaction/reportWalletBalanceTransaction/ReportWalletBalanceTransactionController");
 const ReportWalletDailyBalanceTransactionController = require("../usecases/transaction/reportWalletDailyBalanceTransaction/ReportWalletDailyBalanceTransactionController");
+const ReportWalletCategoryTransactionController = require("../usecases/transaction/reportWalletCategoryTransaction/ReportWalletCategoryTransactionController");
 
 const reportUserTotalTransactionController =
     new ReportUserTotalTransactionController();
@@ -13,6 +14,8 @@ const reportWalletBalanceTransactionController =
     new ReportWalletBalanceTransactionController();
 const reportWalletDailyBalanceTransactionController =
     new ReportWalletDailyBalanceTransactionController();
+const reportWalletCategoryTransactionController =
+    new ReportWalletCategoryTransactionController();
 
 transactionReportRoutes.get(
     "/usertotal",
@@ -30,6 +33,12 @@ transactionReportRoutes.get(
     "/dailybalance",
     [AuthenticationMiddleware.verifyToken],
     reportWalletDailyBalanceTransactionController.report
+);
+
+transactionReportRoutes.get(
+    "/category",
+    [AuthenticationMiddleware.verifyToken],
+    reportWalletCategoryTransactionController.report
 );
 
 module.exports = transactionReportRoutes;
