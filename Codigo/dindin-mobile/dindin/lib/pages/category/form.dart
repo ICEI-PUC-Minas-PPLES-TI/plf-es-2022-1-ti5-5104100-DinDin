85943660fdf0fb254ae0d0af8179a4f89650a384
Guilherme Gabriel <guilherme.gabriel.532@gmail.com>
2022-05-25 22:37:21 -0300

    fix category routes in flutter app

--- Codigo/dindin-mobile/dindin/lib/pages/category/form.dart
+++ Codigo/dindin-mobile/dindin/lib/pages/category/form.dart
@@ -11,7 +11,8 @@ class CategoryForm extends StatefulWidget {
   final Category? category;
   final int walletId;
 
-  const CategoryForm(this.walletId, this.category, {Key? key}) : super(key: key);
+  const CategoryForm(this.walletId, this.category, {Key? key})
+      : super(key: key);
 
   @override
   State<StatefulWidget> createState() {
@@ -27,11 +28,14 @@ class _CategoryFormState extends State<CategoryForm> {
   final TextEditingController _descriptionController = TextEditingController();
 
   void createCategory() async {
-    var url = ApiURL.baseUrl + "/category";
-    final Uri uri = Uri.parse(url);
-    var token = await ApiURL.getToken();
+    var url = "/wallet/" + widget.walletId.toString() + "/category";
     try {
-      var response = await http.post(uri, headers: {'Authorization': token}, body: {'description' : _descriptionController.text, 'type': _catType, 'color': _catColor, 'wallet_id': widget.walletId.toString() });
+      var response = await ApiURL.post(url, body: {
+        'description': _descriptionController.text,
+        'type': _catType,
+        'color': _catColor,
+        'wallet_id': widget.walletId.toString()
+      });
       var status = response.statusCode;
       if (status == 201) {
         var json = jsonDecode(response.body);
@@ -45,11 +49,16 @@ class _CategoryFormState extends State<CategoryForm> {
   }
 
   void editCategory() async {
-    var url = ApiURL.baseUrl + "/category/"+ widget.category!.id.toString();
-    final Uri uri = Uri.parse(url);
-    var token = await ApiURL.getToken();
+    var url = "/wallet/" +
+        widget.walletId.toString() +
+        "/category/" +
+        widget.category!.id.toString();
     try {
-      var response = await http.put(uri, headers: {'Authorization': token}, body: {'description' : _descriptionController.text, 'type': _catType, 'color': _catColor });
+      var response = await ApiURL.put(url, body: {
+        'description': _descriptionController.text,
+        'type': _catType,
+        'color': _catColor
+      });
       var status = response.statusCode;
       if (status == 200) {
         var json = jsonDecode(response.body);
@@ -63,11 +72,12 @@ class _CategoryFormState extends State<CategoryForm> {
   }
 
   void deleteCategory() async {
-    var url = ApiURL.baseUrl + "/category/"+ widget.category!.id.toString();
-    final Uri uri = Uri.parse(url);
-    var token = await ApiURL.getToken();
+    var url = "/wallet/" +
+        widget.walletId.toString() +
+        "/category/" +
+        widget.category!.id.toString();
     try {
-      var response = await http.delete(uri, headers: {'Authorization': token}, body: {'description' : _descriptionController.text, 'type': _catType, 'color': _catColor });
+      var response = await ApiURL.delete(url);
       var status = response.statusCode;
       if (status == 204) {
         Navigator.of(context).pop();
@@ -364,12 +374,11 @@ class _CategoryFormState extends State<CategoryForm> {
                           primary: Theme.of(context).primaryColor,
                         ),
                         onPressed: () {
-                          if(widget.category == null) {
+                          if (widget.category == null) {
                             createCategory();
                           } else {
                             editCategory();
                           }
-
                         },
                       ),
                     ),
--- Codigo/dindin-mobile/dindin/lib/pages/category/list.dart
+++ Codigo/dindin-mobile/dindin/lib/pages/category/list.dart
@@ -10,7 +10,7 @@ import 'package:http/http.dart' as http;
 
 class ListCategories extends StatefulWidget {
   int walletId;
-  ListCategories(this.walletId,{Key? key}) : super(key: key);
+  ListCategories(this.walletId, {Key? key}) : super(key: key);
 
   @override
   _ListCategoriesState createState() => _ListCategoriesState();
@@ -27,7 +27,7 @@ class _ListCategoriesState extends State<ListCategories> {
     getCategories();
   }
 
-  void getCategories() async{
+  void getCategories() async {
     List<Category> l1 = await fetchCategories('IN');
     List<Category> l2 = await fetchCategories('OUT');
     setState(() {
@@ -38,20 +38,26 @@ class _ListCategoriesState extends State<ListCategories> {
 
   Future<List<Category>> fetchCategories(String type) async {
     List<Category> categoryList = <Category>[];
-    var url = ApiURL.baseUrl + "/category?wallet_id="+widget.walletId.toString() + '&type=' + type;
-    final Uri uri = Uri.parse(url);
-    var token = await ApiURL.getToken();
+    var url = "/wallet/${widget.walletId.toString()}/category?wallet_id=" +
+        widget.walletId.toString() +
+        '&type=' +
+        type;
 
     try {
-      var response = await http.get(uri, headers: {'Authorization': token});
+      var response = await ApiURL.get(url);
       var status = response.statusCode;
-      if(status == 200) {
+      if (status == 200) {
         var json = jsonDecode(response.body);
         json['categories'].forEach((row) => {
-          categoryList.add(Category(id: num.parse(row['id'].toString()), userId: num.parse(row['user_id'].toString()), walletId: num.parse(row['wallet_id'].toString()), description: row['description'], type: type, color: row['color'] )),
-          print(row)
-        });
-
+              categoryList.add(Category(
+                  id: num.parse(row['id'].toString()),
+                  userId: num.parse(row['user_id'].toString()),
+                  walletId: num.parse(row['wallet_id'].toString()),
+                  description: row['description'],
+                  type: type,
+                  color: row['color'])),
+              print(row)
+            });
       }
     } catch (e) {
       print("deu nao");
@@ -59,7 +65,6 @@ class _ListCategoriesState extends State<ListCategories> {
     }
     return categoryList;
   }
-
 
   TabBar get _tabBar => const TabBar(
         indicatorColor: Colors.grey,
@@ -95,7 +100,7 @@ class _ListCategoriesState extends State<ListCategories> {
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                    builder: (context) => CategoryForm(widget.walletId,null)),
+                    builder: (context) => CategoryForm(widget.walletId, null)),
               ).then((value) => getCategories());
             },
             child: const Icon(Icons.add),
