a1366ea046f6f68fb608c07e5820a6cade0a5513
Guilherme Gabriel <guilherme.gabriel.532@gmail.com>
2022-06-05 13:32:14 -0300

    fixed reading unexisting wallet_id

--- Codigo/dindin-api/app/usecases/goal/updateGoal/UpdateGoalUseCase.js
+++ Codigo/dindin-api/app/usecases/goal/updateGoal/UpdateGoalUseCase.js
@@ -7,7 +7,7 @@ class UpdateGoalUseCase {
         const findGoalUseCase = new FindGoalUseCase();
         const goal = await findGoalUseCase.find(id);
 
-        let status = goal.status;
+        let status = undefined;
 
         // when a goal is updated to saving and the expire_at is not past,
         // the goal status should be updated to "PENDING"
@@ -39,7 +39,7 @@ class UpdateGoalUseCase {
             });
 
         if (type === "A")
-            new GoalService().updateAchievemetWalletGoals(wallet_id);
+            new GoalService().updateAchievemetWalletGoals(goal.wallet_id);
 
         return goal;
     }
