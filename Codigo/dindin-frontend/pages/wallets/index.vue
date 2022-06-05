05ecbdc5dbfbbdfc9615791f35b46dbc2a9a4664
Henrique Monteiro <henrique.mont011@gmail.com>
2022-06-05 18:37:56 -0300

    Fix console errors

--- Codigo/dindin-frontend/pages/wallets/index.vue
+++ Codigo/dindin-frontend/pages/wallets/index.vue
@@ -300,6 +300,7 @@ export default {
             modalEdit: false,
             showMembersModal: false,
             walletToShowMembersId: "",
+            walletToShowMembers: null,
         };
     },
     async fetch() {
