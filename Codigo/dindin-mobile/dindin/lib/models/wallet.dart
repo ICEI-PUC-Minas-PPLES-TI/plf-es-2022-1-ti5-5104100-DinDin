f84e109aec5d09d157615b90341f44e17cb9f93e
Lucas Angelo O M Rocha <lcs2001_@hotmail.com>
2022-06-06 00:55:01 -0300

    Add ownerUser at Wallet model

--- Codigo/dindin-mobile/dindin/lib/models/wallet.dart
+++ Codigo/dindin-mobile/dindin/lib/models/wallet.dart
@@ -1,3 +1,5 @@
+import 'package:dindin/models/user.dart';
+
 class Wallet {
   final num id;
   final String? description;
@@ -6,26 +8,27 @@ class Wallet {
   final String? createdAt;
   final String? updatedAt;
   final String? deletedAt;
+  final User? owenerUser;
 
-  Wallet({
-    required this.id,
-    required this.description,
-    required this.currentValue,
-    required this.shared,
-    required this.createdAt,
-    required this.updatedAt,
-    required this.deletedAt,
-  });
+  Wallet(
+      {required this.id,
+      required this.description,
+      required this.currentValue,
+      required this.shared,
+      required this.createdAt,
+      required this.updatedAt,
+      required this.deletedAt,
+      this.owenerUser});
 
   factory Wallet.fromJson(Map<String, dynamic> json) {
     return Wallet(
-      id: int.parse(json['id']),
-      description: (json['description']).toString(),
-      currentValue: json['initial_value'],
-      shared: json['shared'] ? 1 : 0,
-      createdAt: json['created_at'].toString(),
-      updatedAt: json['updated_at'].toString(),
-      deletedAt: json['deleted_at'],
-    );
+        id: int.parse(json['id']),
+        description: (json['description']).toString(),
+        currentValue: json['initial_value'],
+        shared: json['shared'] ? 1 : 0,
+        createdAt: json['created_at'].toString(),
+        updatedAt: json['updated_at'].toString(),
+        deletedAt: json['deleted_at'],
+        owenerUser: User.fromJson(json['owner_user']));
   }
 }
